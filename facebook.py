43f53d9c1e1b1aea9e0e831041a22ec4856b657d
parrot <3417947630@qq.com>
2019-01-24 11:09:11 +0800

    facebook crawl

---
+++ facebook.py
@@ -0,0 +1,124 @@
+import re
+import time
+
+import pymongo
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from lxml import etree
+from urllib.parse import quote
+
+MONGO_URL = 'localhost'
+MONGO_DB = 'facebook'
+# browser = webdriver.Chrome()
+# browser = webdriver.PhantomJS(service_args=SERVICE_ARGS)
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--proxy-server=socks5://127.0.0.1:1080")
+browser = webdriver.Chrome(chrome_options=chrome_options)
+
+wait = WebDriverWait(browser, 10)
+client = pymongo.MongoClient(MONGO_URL)
+db = client[MONGO_DB]
+
+base_url = 'https://www.facebook.com/pg'
+KEYWORDS = ['python','java','c++','Mac','Liunx','Facebook']
+
+
+def login():
+    browser.get('https://www.facebook.com')
+    browser.find_element_by_id('email').send_keys('wutong8773@163.com')
+    browser.find_element_by_id('pass').send_keys('wt008911wt008911')
+    browser.find_element_by_id('login_form').submit()
+    browser.implicitly_wait(10)
+
+
+def search_key(KEYWORD):
+    url = 'https://www.facebook.com/search/str/' + quote(KEYWORD) + '/keywords_pages?epa=SEE_MORE'
+    browser.get(url)
+    while 1:
+        time.sleep(6)
+        js = "var q=document.documentElement.scrollTop=10000"
+        browser.execute_script(js)
+        if '已经到底啦' in browser.page_source:
+            break
+
+    doc = etree.HTML(browser.page_source)
+    divs = doc.xpath("//a[@class='_32mo']/@href")
+    for url in divs:
+        parse(url)
+
+def index_page():
+ 
+    # 抓取索引页
+    print('---------')
+    try:
+        # js = "var q=document.documentElement.scrollTop=10000"
+        # browser.execute_script(js)
+        doc = etree.HTML(browser.page_source)
+        divs = doc.xpath("//a[@class='_32mo']/@href")
+        for url in divs:
+            parse(url)
+        # submit = wait.until(
+        #     EC.element_to_be_clickable((By.CSS_SELECTOR, '#mainsrp-pager div.form > span.btn.J_Submit')))
+        # input.clear()
+        # input.send_keys(page)
+        # submit.click()
+        # wait.until(
+        #     EC.text_to_be_present_in_element((By.CSS_SELECTOR, '#mainsrp-pager li.item.active > span'), str(page)))
+        # wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.m-itemlist .items .item')))
+        #
+    except TimeoutException:
+        index_page()
+
+def parse(url):
+
+    browser.get(url)
+    doc = etree.HTML(browser.page_source)
+    jianjie = base_url + doc.xpath("//div[@class='_2yaa' and contains(@data-key,'tab_about')]/a/@href")[0]
+    browser.get(jianjie)
+    time.sleep(2)
+    doc = etree.HTML(browser.page_source)
+    # ret = re.compile(r'<div class="_50f4">(.*?@.*?)</div>')
+    # results = re.findall(ret,browser.page_source)
+    results = doc.xpath("//div[@class='_50f4']/text()")
+    if len(results) > 0:
+        for result in results:
+            if '@' in result:
+                print(result)
+
+
+def scroll_until_loaded():
+    el = browser.find_element_by_id("facebook")
+    js = "var q=document.documentElement.scrollTop=10000"
+    browser.execute_script(js)
+    time.sleep(2)
+    browser.execute_script(js)
+    time.sleep(2)
+    browser.execute_script(js)
+    time.sleep(2)
+    print(browser.page_source)
+    # check_height = browser.execute_script("return document.body.scrollHeight;")
+    # while True:
+    #     browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    #     try:
+    #         wait.until(
+    #             lambda driver: browser.execute_script("return document.body.scrollHeight;") > check_height)
+    #         check_height = browser.execute_script("return document.body.scrollHeight;")
+    #     except TimeoutException:
+    #         break
+
+def close():
+    browser.close()
+
+def main():
+    login()
+    for key in KEYWORDS:
+        search_key(key)
+    close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ road.py
@@ -0,0 +1,134 @@
+
+from selenium import webdriver
+from sockshandler import SocksiPyHandler
+import socks
+from urllib.request import build_opener
+from lxml import etree
+import pymongo
+import time
+import re
+
+class Road():
+
+    def __init__(self):
+        self.base_url = 'http://silkroad4n7fwsrw.onion/'
+        self.conn = pymongo.MongoClient('127.0.0.1',27017)
+        self.db = self.conn['Road']
+
+        self.chromeOptions = webdriver.ChromeOptions()
+
+        # 设置代理
+        self.chromeOptions.add_argument("--proxy-server=socks5://127.0.0.1:9150")
+        # 一定要注意，=两边不能有空格
+        self.browser = webdriver.Chrome(chrome_options=self.chromeOptions)
+        self.browser.get(self.base_url)
+        time.sleep(20)
+        print(self.browser.page_source)
+        self.proxy_handler = SocksiPyHandler(proxytype=socks.SOCKS5, proxyaddr='127.0.0.1', proxyport=9150)
+        self.opener = build_opener(self.proxy_handler)
+
+
+    def get_all(self,response):
+
+        if 'CAPTCHA' in response:
+            img = r'<img src="(.*?)" />'
+            img_link = re.findall(img,response)[0]
+            print(img_link)
+            img_html = self.opener.open(img_link,timeout=40).read()
+            with open('captcha.png','wb') as f:
+                f.write(img_html)
+
+            rr = self.browser.find_element_by_xpath("//form/input[1]")
+            key = input('keys:')
+            rr.send_keys(key)
+            submit = self.browser.find_element_by_xpath("//form/input[2]")
+            submit.click()
+            # time.sleep(30)
+            self.get_all(response)
+        else:
+            doc = etree.HTML(response)
+                # print(doc)
+            # divs = doc.xpath("//tr/td[1]/div[@id='vp']//tr/td[2]/div")
+            # # print(divs)
+            # for div in divs:
+            #     link = div.xpath("./a[1]/@href")[0]
+            #
+            #     title = div.xpath("./a[1]/text()")[0]
+            #     Price = div.xpath("./b[2]/text()")[0].rstrip('/')
+            #     view_list = div.xpath("./a[2]/@href")[0]
+            #     Category = div.xpath("./a[3]/text()")[0]
+            #     ShipsFrom= div.xpath("./span[@id='ah_ships']/text()")[0]
+            #     print(title,Price,link,view_list,Category,ShipsFrom,sep='\n')
+            #     print('\n\n')
+            divs = doc.xpath("//tr/td[2]/div[@id='vp']/a/@href")
+            for div in divs:
+                sub_link = self.base_url + div
+                print(sub_link)
+                self.browser.get(sub_link)
+                time.sleep(15)
+                self.get_content(self.browser.page_source)
+
+    def get_content(self,response):
+        # self.browser.get(link)
+        doc = etree.HTML(response)
+        divs = doc.xpath("//tr/td[1]/div[@id='vp']//tr/td[2]/div")
+        if divs:
+            # print(divs)
+            for div in divs:
+                item = {}
+
+                item['link'] = self.base_url +  div.xpath("./a[1]/@href")[0]
+
+                item['title'] = div.xpath("./a[1]/text()")[0]
+                item['Price'] = div.xpath("./b[2]/text()")[0].rstrip(' /')
+                item['view_list'] = self.base_url + div.xpath("./a[2]/@href")[0]
+                item['Category'] = self.base_url +  div.xpath("./a[3]/text()")[0]
+                item['ShipsFrom'] = div.xpath("./span[@id='ah_ships']/text()")[0]
+                # print(title,Price,link,view_list,Category,ShipsFrom,sep='\n')
+                print(item)
+                print('\n\n')
+                if self.db['SilkRoad'].insert(item):
+                    print('Sueecss saved to Mongo ...... ')
+                else:
+                    print('FAILED')
+                    
+        while True:
+            try:
+                next_link = self.browser.find_element_by_xpath("//tr/td[1]/center/form/input[@value='Next Page']")
+                if next_link:
+                    next_link.click()
+                    time.sleep(10)
+                    self.get_content(self.browser.page_source)
+            except Exception:
+
+                return None
+
+    def web(self):
+
+        if 'CAPTCHA' in self.browser.page_source:
+            img = r'<img src="(.*?)" />'
+            img_link = re.findall(img,self.browser.page_source)[0]
+            print(img_link)
+            img_html = self.opener.open(img_link,timeout=40).read()
+            with open('captcha.png','wb') as f:
+                f.write(img_html)
+
+            rr = self.browser.find_element_by_xpath("//form/input[1]")
+            key = input('keys:')
+            rr.send_keys(key)
+            submit = self.browser.find_element_by_xpath("//form/input[2]")
+            submit.click()
+            # time.sleep(30)
+            self.get_all(self.browser.page_source)
+        else:
+            self.get_all(self.browser.page_source)
+        # 退出，清除浏览器缓存
+        self.browser.quit()
+
+    def run(self):
+        self.web()
+
+if __name__ == '__main__':
+    r = Road()
+    r.run()
+
